aa1746d609aabaa15b311f672539f45113fe2515
Tai55 <jesus.393hernandez@gmail.com>
2022-02-02 13:13:59 +0100

    Comienzo de Proyecto

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PROYECTO_CONSOLA
+{
+    class Program
+    {
+        static void Main(string[] args) //void main es el comienzo - método
+        {
+            Console.WriteLine ("Escriba la primera variable");
+            String variable1;
+            variable1 = Console.ReadLine();
+            Console.WriteLine ("El texto introducido es:" + variable1);
+
+            Console.WriteLine ("Escriba la segunda variable");
+            String variable2;
+            variable2 = Console.ReadLine();
+            Console.WriteLine ("El texto introducido es:" + variable2);
+    
+
+            string.Equals(variable1,variable2); 
+            //bool resultado = mensajeDesalida1.equals(mensajeDeSalida);
+
+            
+            bool resultado = string.Equals(variable1,variable2); 
+            Console.WriteLine(resultado.ToString()); //no hace falta poner el ToString porque coge bool
+           // Console.WriteLine(string.Equals(variable1,variable2));
+
+            
+            Console.ReadLine();
+            
+        }//fin método
+    }//fin clase
+}
